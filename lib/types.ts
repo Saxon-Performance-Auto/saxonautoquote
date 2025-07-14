ac2cb543b6b5df78ae42f6f38dcc5e1f8a79d9a5
leeeeesamo <lmoore0510@gmail.com>
2025-07-14 06:34:53 -0500

    updated lib/types.ts to entirely align wiht schema

--- lib/types.ts
+++ lib/types.ts
@@ -1,26 +1,34 @@
 export type Quote = {
   id: number;
-  customer_id: string;
   job_description: string;
-  labor_cost: number;
-  total_cost: number;
+  laborCost: number;
+  totalCost: number;
+  inspection?: string | null;
+  diagnostics?: string | null;
+  notes?: string | null;
   createdAt: Date;
+  updatedAt: Date;
+  customerId: number;
 };
 
 export type Customer = {
+  id: number;
   name: string;
   phone: string;
-  email: string;
+  email?: string | null;
   vehicle: string;
-  vin: string; ?
-  mileageIn: string;
-  mileageOut: string;
+  vin?: string | null;
+  mileageIn?: string | null;
+  mileageOut?: string | null;
+  createdAt: Date;
+  updatedAt: Date;
 };
 
 export type Part = {
-  id?: number;
-  quote_id: number;
-  part_name: string;
-  part_price: number;
+  id: number;
+  partName: string;
+  price: number;
+  quoteId: number;
+  createdAt: Date;
+  updatedAt: Date;
 };
-
